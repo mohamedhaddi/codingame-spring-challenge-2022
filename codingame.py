7a7f882429fbd708d5afe92753da07e139f51bc6
Mohamed Haddi <m.haddi.95@gmail.com>
2022-05-02 16:02:23 +0000

    Update codingame.py

--- codingame.py
+++ codingame.py
@@ -1,5 +1,5 @@
 """
-Disclaimer: I'm not proud of this code, it's unclean and full of hardcoding and I didn't focus on making it clean, so don't judge T_T
+Disclaimer: I'm not proud of this code, it's unclean and full of hardcoding (some of it I did at last minute) and I didn't focus on making it clean, so don't judge T_T
 """
 
 import sys
